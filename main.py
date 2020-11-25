3d3438c87bd2330f4fe1f41f80aeb0dd3225864a
Thomas Cormier <43412314+thomascormier@users.noreply.github.com>
2020-11-25 13:09:26 +0100

    milestone_2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime as dt, timedelta as td
 
 ########################################################################################
 #                                   Milestone 1
+#
+# We clean and stock our data properly into the pandas dataframe df.
 ########################################################################################
 
 
@@ -10,7 +14,7 @@ def generate_clean_dataframe():
     filename = "data/enron_100K.csv"
     df = pd.read_csv(filename, low_memory=False)
 
-    # Dropping every useless columuns
+    # Dropping every useless columns
     df = df.drop(columns=["Message-ID", "From", "To", "X-cc", "X-bcc", "X-Folder", "X-Origin", "X-FileName",
                           "Cat_1_level_1", "Cat_1_level_2", "Cat_1_weight", "Cat_2_level_1", "Cat_2_level_2",
                           "Cat_2_weight", "Cat_3_level_1", "Cat_3_level_2", "Cat_3_weight", "Cat_4_level_1",
@@ -43,26 +47,74 @@ def generate_clean_dataframe():
     return df
 
 
+df = generate_clean_dataframe()
+print("Data is now cleaned and loaded in a Dataframe.")
+
 ########################################################################################
 #                                   Milestone 2
+#
+# In this milestone, we want to plot graphs that give repartition of mails sent and received, during a day.
+# Also, we want to display how many mails were sent and received every day between 1980 and 2020.
 ########################################################################################
 
 
-def graph_number_of_mail_received_per_hour(df):
-    pass
+def graph_number_of_mail_per_hour(df):
+    repartition_per_hour = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0,
+                            "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0,
+                            "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0,
+                            "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0}
+
+    for index, row in df.iterrows():
+        hour = row['Date'].hour
+        hour_str = str(hour)
+        repartition_per_hour[hour_str] = repartition_per_hour[hour_str] + 1
+
+    # sorted by key, return a list of tuples
+    lists = repartition_per_hour.items()
+    # unpack a list of pairs into two tuples
+    x, y = zip(*lists)
+    # display results
+    plt.plot(x, y)
+    plt.title('Number of mail received and sent per hour')
+    plt.ylabel('Number of mails')
+    plt.xlabel('Hour')
+    plt.grid(True)
+    plt.show()
 
 
-def graph_number_of_mail_sent_per_hour(df):
-    pass
+def graph_number_of_mail_per_day(df):
+    start = '1980-01-01'
+    end = '2021-01-01'
+    sd = dt.strptime(start, '%Y-%m-%d')
+    ed = dt.strptime(end, '%Y-%m-%d')
+    delta = ed - sd
+    repartition_per_day = {}
+    for i in range(delta.days + 1):
+        repartition_per_day[str(sd + td(days=i))[:-9]] = 0
+
+    print('dict initialized')
+
+    # Keep only date part when using pandas.to_datetime
+    df['just_date'] = df['Date'].dt.date
+    for row in df['just_date']:
+        repartition_per_day[str(row)] += 1
+
+    # sorted by key, return a list of tuples
+    lists = repartition_per_day.items()
+    # unpack a list of pairs into two tuples
+    x, y = zip(*lists)
+    print('Data is ready. Start Plotting')
+    # display results
+    plt.plot(x, y)
+    plt.title('Number of mail received and sent per day')
+    plt.ylabel('Number of mails')
+    plt.xlabel('Date')
+    plt.grid(True)
+    plt.show()
 
 
-def graph_number_of_mail_received_per_day(df):
-    pass
-
-
-def graph_number_of_mail_sent_per_day(df):
-    pass
-
+# graph_number_of_mail_per_hour(df)
+# graph_number_of_mail_per_day(df)
 
 ########################################################################################
 #                                   Milestone 3
@@ -96,18 +148,6 @@ def keywords_cloud(df):
 
 
 ########################################################################################
-
-# Milestone 1
-# We clean and stock our data properly into the pandas dataframe df
-df = generate_clean_dataframe()
-
-# TODO Milestone 2
-# In this milestone, we want to plot graphs that give repartition of mails sent and received, during a day.
-# Also, we want to display how many mails were sent and received every day between 1980 and 2020.
-graph_number_of_mail_received_per_hour(df)
-graph_number_of_mail_sent_per_hour(df)
-graph_number_of_mail_received_per_day(df)
-graph_number_of_mail_sent_per_day(df)
 
 # TODO Milestone 3
 # In this Milestone, we worked on the most common topics in the subject and the content of the mails.
